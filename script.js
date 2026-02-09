431b1d2f51d4fab31947e1c0a90a970d655e17ca
LehiSaid <lehi_said@outlook.com>
2026-02-09 20:33:16 -0300

    feat: adiciona melhor de 3, sons e mensagens finais

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const result = document.querySelector('.result');
 const humanScore = document.querySelector('#human-score');
 const machineScore = document.querySelector('#machine-score');
+const finalMessage = document.getElementById("final-message");
+const MAX_WINS = 2; // melhor de 3
+const soundWin = new Audio("sounds/win.mp3");
+const soundLose = new Audio("sounds/lose.mp3");
+const soundDraw = new Audio("sounds/draw.mp3");
+
+
 
 let humanScoreNumber = 0
 let machineScoreNumber = 0
@@ -24,17 +31,22 @@ const playTheGame = (human, machine) => {
     console.log('Humano: ' + human + " Máquina: " + machine)
 
     if (human === machine) {
-        result.textContent = "Empate!"
+        result.textContent = "Empate!";
+        soundDraw.play();
     } else if ((human === 'rock' && machine === 'scissors') ||
         (human === 'paper' && machine === 'rock') ||
         (human === 'scissors' && machine === 'paper')) {
         humanScoreNumber++;
         humanScore.innerHTML = humanScoreNumber;
         result.innerHTML = "Você ganhou!"
+        soundWin.play();
+        checkWinner();
     } else {
         machineScoreNumber++;
         machineScore.innerHTML = machineScoreNumber;
         result.innerHTML = "Você perdeu!"
+        soundLose.play();
+        checkWinner();
     }
 }
 
@@ -50,12 +62,19 @@ const animeBackgrounds = [
     "img/dbz3.jpg",
 ];
 
+animeBackgrounds.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
+
 
 let currentBg = 0;
 
 function changeBackground() {
-    document.body.style.backgroundImage =
-        `url('${animeBackgrounds[currentBg]}')`;
+    document.body.style.setProperty(
+        "--bg",
+        `url('${animeBackgrounds[currentBg]}')`
+    );
 
     currentBg = (currentBg + 1) % animeBackgrounds.length;
 }
@@ -64,6 +83,37 @@ function changeBackground() {
 changeBackground();
 
 // troca a cada 8 segundos
-setInterval(changeBackground, 3000);
+setInterval(changeBackground, 6000);
+
+
+function checkWinner() {
+    if (humanScoreNumber === MAX_WINS) {
+        showFinalMessage("🏆 Você venceu a melhor de 3!", "win");
+        resetGame();
+    }
+
+    if (machineScoreNumber === MAX_WINS) {
+        showFinalMessage("🤖 A Alexa venceu a melhor de 3!", "lose");
+        resetGame();
+    }
+}
+
+function showFinalMessage(text, type) {
+    finalMessage.textContent = text;
+    finalMessage.className = `final-message show ${type}`;
+
+    setTimeout(() => {
+        finalMessage.className = "final-message";
+    }, 3000);
+}
+
+function resetGame() {
+    humanScoreNumber = 0;
+    machineScoreNumber = 0;
+
+    humanScore.textContent = 0;
+    machineScore.textContent = 0;
+    result.textContent = "Novo jogo iniciado!";
+}
 
 
